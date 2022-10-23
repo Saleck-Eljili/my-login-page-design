bc79861d4aef43043b4b3213b2809c6bf2c7b0e1
Saleck-Eljili <salek.eljili@gmail.com>
2022-10-23 13:11:19 +0200

    feat: add html content

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="login">
+        <div className="card">
+            <div className="left">
+                <h1>Hello word</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque nesciunt debitis adipisci obcaecati consectetur illo vel aut voluptatibus corrupti quia autem sequi sed maiores, quae omnis tempora ad quaerat molestiae?</p>
+                <span>Don't you have an account</span>
+                <button>Register</button>
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder='username'/>
+                    <input type="password" placeholder='password'/>
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
