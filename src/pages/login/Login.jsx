ad256f4660159de81ba06dd2d856f333304a09e6
Saleck-Eljili <salek.eljili@gmail.com>
2022-10-24 12:33:14 +0200

    feat: add style to html content

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import './login.scss'
 
 function Login() {
   return (
     <div className="login">
-        <div className="card">
-            <div className="left">
-                <h1>Hello word</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque nesciunt debitis adipisci obcaecati consectetur illo vel aut voluptatibus corrupti quia autem sequi sed maiores, quae omnis tempora ad quaerat molestiae?</p>
-                <span>Don't you have an account</span>
-                <button>Register</button>
-            </div>
-            <div className="right">
-                <h1>Login</h1>
-                <form>
-                    <input type="text" placeholder='username'/>
-                    <input type="password" placeholder='password'/>
-                    <button>Login</button>
-                </form>
-            </div>
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+          
+            <button>Register</button>
+          
         </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button >Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
